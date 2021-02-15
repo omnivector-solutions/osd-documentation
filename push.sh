27b9d65facd5dfcfb90f88ed5b04669fa55cdf97
jamesbeedy <jamesbeedy@gmail.com>
2021-02-15 16:57:03 -0700

    add github pages publishing workflow for the OSD docs
    * thank you @OSC
    * push.sh - https://github.com/OSC/ood-documentation/blob/0f4f551320064d439181c5e6391682d96ca12a0f/push.sh
    * publish.yaml - https://github.com/OSC/ood-documentation/blob/0f4f551320064d439181c5e6391682d96ca12a0f/.github/workflows/publish.yml

---
+++ push.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+GITHUB_ORG="omnivector-solutions"
+NAME="omnivector-qa"
+EMAIL="admin@omnivector.solutions"
+
+# Update git configuration
+git config --global user.name "${NAME}"
+git config --global user.email "${EMAIL}"
+
+repo=${1}
+workdir="$(mktemp -d)"
+builddir="${PWD}"
+
+# Clone the gh-pages branch outside of the current repo and cd into it
+cd "${workdir}"
+git clone -b gh-pages "https://${GITHUB_TOKEN}@github.com/${GITHUB_ORG}/${repo}.git" gh-pages
+cd gh-pages
+
+REF=${GITHUB_HEAD_REF:-${GITHUB_REF#refs/heads/}}
+
+# Clean up old build and copy in the new built html
+rm -fr "${REF}"
+cp -R "${builddir}/build/html" "${REF}"
+
+# Add and commit changes
+git add -A .
+git commit -m "[ci skip] Autodoc commit for ${GITHUB_SHA}."
+# -q is very important, otherwise you leak your GITHUB_TOKEN
+git push -q -f origin gh-pages
